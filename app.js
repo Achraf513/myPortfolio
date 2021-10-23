2ccd7501bdf0103a64c35e31534309d436102755
Darken513 <affesachraf70@gmail.com>
2021-10-23 18:35:48 +0100

    Prototype01

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static('./portfolio/'))
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html',{root:__dirname})
+});
+app.get('/downloadCv', (req, res) => {
+    res.sendFile('Achraf_Affes_CV.pdf',{root:__dirname})
+});
+
+app.listen(port, () => {
+    console.log("Server is listening on port "+port);
+});
